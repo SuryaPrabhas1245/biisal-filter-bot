86ff2f753ca2c921f0a622b003e56eaa3eec1b3e
SuryaPrabhas1245 <85777749+SuryaPrabhas1245@users.noreply.github.com>
2024-11-07 14:55:39 +0530

    Update botHandeling.py

--- plugins/components/botHandeling.py
+++ plugins/components/botHandeling.py
@@ -55,9 +55,9 @@ async def set_stream_link(client, message):
             await db.set_stream_link(link)
             await message.reply("Successfully set stream link!")
         else:
-            await message.reply("Usage: /stream https://t.me/bisal_files or http://t.me/bisal_files")
+            await message.reply("Usage: /stream https://t.me/BottyUpdates or http://t.me/BottyUpdates")
     except IndexError:
-        await message.reply("Usage: /stream https://t.me/bisal_files or http://t.me/bisal_files")
+        await message.reply("Usage: /stream https://t.me/BottyUpdates or http://t.me/BottyUpdates")
         return
 
 
@@ -90,15 +90,15 @@ async def m_grp(client, message):
     links = []
     link = await client.ask(message.chat.id ,"send me your pm search grp link or send /skiplink to skip , default is bisal_files")
     if link.text == "/skiplink":
-        links.append("https://t.me/bisal_files")
+        links.append("https://t.me/BottyUpdates")
     else:
         if checkIfLinkIsValid(link.text):
             links.append(link.text)
         else:
             await message.reply("Invalid link")
-    link1 = await client.ask(message.chat.id ,"send me your movies grp link or send /skiplink to skip . default is bisal_files")
+    link1 = await client.ask(message.chat.id ,"send me your movies grp link or send /skiplink to skip . default is BottyUpdates")
     if link1.text == "/skiplink":
-        links.append("https://t.me/bisal_files")
+        links.append("https://t.me/BottyUpdates")
     else:
         if checkIfLinkIsValid(link1.text):
             links.append(link1.text)
